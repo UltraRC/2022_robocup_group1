234c3409314e6b83b14f8896d4c3276b6c08129e
UltraRC <ultrapotatoemasher@gmail.com>
2022-08-22 09:49:41 +1200

    update

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,17 @@
 #include "motor.h"
 #include "PPMReader.h"
 
+#include <PWMServo.h> // REMOVE
+
+#define L_ARM_PIN 30
+#define R_ARM_PIN 31
+
 #define RC_IN 23
 #define NUM_CHANNELS 6
+
+PWMServo arm_left;
+PWMServo arm_right;
+PWMServo thicc_motor;
 
 PPMReader ppm(RC_IN, NUM_CHANNELS);
 int32_t channels[NUM_CHANNELS];
@@ -13,7 +22,10 @@ void update_channels();
 void setup()
 {
     init_motors();
-    //Serial.begin(9600);
+    arm_left.attach(L_ARM_PIN); // Servo pins : 30 - 33
+    arm_right.attach(R_ARM_PIN);
+    thicc_motor.attach(32);
+    Serial.begin(9600);
 }
 
 void loop()
@@ -23,6 +35,13 @@ void loop()
     //Serial.print("\t");
     //Serial.print(channels[2]);
     //Serial.print("\n");
+
+    for(uint8_t i=0; i<6; i++)
+    {
+        Serial.print("Channel "); Serial.print(i); Serial.print(": "); Serial.print(channels[i]); Serial.print("\t");
+    }
+    Serial.println();
+
     delay(20);
     int32_t left_speed = 0.5*channels[1] - 0.5*channels[2];
     int32_t right_speed = -0.5*channels[1] - 0.5*channels[2];
@@ -30,6 +49,13 @@ void loop()
     right_speed *= 2;
     set_motor_speed_left(left_speed);
     set_motor_speed_right(right_speed);
+
+    int32_t servo_set = map(channels[0], -100, 100, 0, 180);
+    //arm_left.write(servo_set);
+    //arm_right.write(180-servo_set);
+    //thicc_motor.write(90);
+
+    //Serial.println(servo_set);
 }
 
 void update_channels()
